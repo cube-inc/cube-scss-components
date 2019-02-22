587becfe99eb87ad9eae619294c60a40dcaa58a5
Fred Gomez <fred.titao@gmail.com>
2019-02-23 00:45:22 +0100

    Improves Exemple

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -8,32 +8,35 @@
 </template>
 
 <script>
+import vnodesToString from '@/services/vnodeToString'
 import hljs from 'highlight.js/lib/highlight'
 
 export default {
   name: 'Example',
+  props: {
+    html: String
+  },
   data () {
     return {
       code: null
     }
   },
   methods: {
-    highlight () {
-      this.code = this.$refs.preview.innerHTML
-        .replace(/^\s+/, '')
-        .replace(/ data-v-.+?=""/g, '')
+    format () {
+      this.code = vnodesToString(this.$slots.default)
       this.$nextTick(() => {
         hljs.highlightBlock(this.$refs.code)
       })
     }
   },
   mounted () {
-    this.highlight()
+    this.format()
   }
 }
 </script>
 
 <style lang="scss" scoped>
+$bg-color: rgba($brand-info, .05);
 .exemple {
   margin: 1em 0;
   border: 1px solid #eeeeee;
@@ -42,11 +45,18 @@ export default {
 .preview {
   padding: 32px;
   background-color: white;
+  background-image:
+    linear-gradient(45deg, $bg-color 25%, transparent 25%, transparent 75%, $bg-color 75%),
+    linear-gradient(45deg, $bg-color 25%, transparent 25%, transparent 75%, $bg-color 75%);
+  background-size: 10px 10px;
+  background-position: 0 0, 5px 5px;
 }
 .code {
   display: block;
   padding: 32px;
   background-color: #eeeeee;
-  white-space: pre-line;
+  white-space: pre-wrap;
+  font-size: 12px;
+  font-family: Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
 }
 </style>
