a0d18b8e3bd1b1b47c5c744afd9dde42106e399b
Fred Gomez <fred.titao@gmail.com>
2019-02-23 17:13:43 +0100

    Improves Pagination Example

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -6,8 +6,9 @@
         <a class="page-item page-button">Prev</a>
         <a class="page-item page-button">1</a>
         <a class="page-item page-button">2</a>
-        <div class="page-item">3</div>
-        <div class="page-item page-info">Info</div>
+        <div class="page-item page-info">Page 4/10</div>
+        <div class="page-item">Inactive</div>
+        <div class="page-item">10</div>
         <a class="page-item page-button">Next</a>
       </nav>
     </Example>
