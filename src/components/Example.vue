a550f45d7aa2afa15eb7b2cc6a913c9b942ac05f
Fred Gomez <fred.titao@gmail.com>
2019-02-23 23:31:08 +0100

    Improves Example

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="exemple">
-    <div class="preview" ref="preview">
+  <div class="example">
+    <div class="example-preview" ref="preview">
       <slot/>
     </div>
-    <code class="code" ref="code">{{ code }}</code>
+    <code class="example-code" ref="code">{{ code }}</code>
   </div>
 </template>
 
@@ -37,28 +37,39 @@ export default {
 
 <style lang="scss" scoped>
 $bg-color: rgba($brand-info, .05);
-.exemple {
+$bg-square-size: 10px;
+.example {
   margin: 1em 0;
   border: 1px solid #eeeeee;
-  border-radius: 2px;
-}
-.preview {
-  padding: 32px;
-  background-color: white;
-  background-image:
-    linear-gradient(45deg, $bg-color 25%, transparent 25%, transparent 75%, $bg-color 75%),
-    linear-gradient(45deg, $bg-color 25%, transparent 25%, transparent 75%, $bg-color 75%);
-  background-size: 10px 10px;
-  background-position: 0 0, 5px 5px;
-  overflow: scroll;
-  -webkit-overflow-scrolling: touch;
-}
-.code {
-  display: block;
-  padding: 32px;
-  background-color: #eeeeee;
-  white-space: pre-wrap;
-  font-size: 12px;
-  font-family: Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+  border-radius: $component-border-radius;
+  overflow: hidden;
+  &-preview-scroll {
+    .example-preview {
+      overflow: scroll;
+      -webkit-overflow-scrolling: touch;
+    }
+  }
+  &-preview {
+    padding: 32px;
+    background-color: white;
+    background-image:
+      linear-gradient(45deg, $bg-color 25%, transparent 25%, transparent 75%, $bg-color 75%),
+      linear-gradient(45deg, $bg-color 25%, transparent 25%, transparent 75%, $bg-color 75%);
+    background-size: $bg-square-size $bg-square-size;
+    background-position: 0 0, ($bg-square-size / 2) ($bg-square-size / 2);
+  }
+  &-code {
+    display: block;
+    padding: 32px;
+    background-color: $component-bg-color-light;
+    white-space: pre-wrap;
+    font-size: 12px;
+    font-family: Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+  }
+  &-dark {
+    .example-preview {
+      background-color: $gray-base;
+    }
+  }
 }
 </style>
