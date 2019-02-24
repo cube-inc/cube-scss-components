5ffcdf7437019e2432c110efbfe4512edc966261
Fred Gomez <fred.titao@gmail.com>
2019-02-24 19:00:00 +0100

    Improves Badges

--- src/components/Badges.vue
+++ src/components/Badges.vue
@@ -3,11 +3,11 @@
     <nav><a href="#Toc">Top</a></nav>
     <h2>Badges</h2>
     <Example>
-      <p>.badge <span class="badge">99</span></p>
-      <p>.badge-success <span class="badge badge-success">1</span></p>
-      <p>.badge-info <span class="badge badge-info">2</span></p>
-      <p>.badge-warning <span class="badge badge-warning">3</span></p>
-      <p>.badge-danger <span class="badge badge-danger">4</span></p>
+      <p><span class="badge">99</span> .badge</p>
+      <p><span class="badge badge-success">1</span> .badge-success</p>
+      <p><span class="badge badge-info">2</span> .badge-info</p>
+      <p><span class="badge badge-warning">3</span> .badge-warning</p>
+      <p><span class="badge badge-danger">4</span> .badge-danger</p>
     </Example>
   </section>
 </template>
