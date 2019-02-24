ba6d9e9be671efbfb85470a7eec71833532ef05b
Fred Gomez <fred.titao@gmail.com>
2019-02-24 19:00:11 +0100

    Improves Labels

--- src/components/Labels.vue
+++ src/components/Labels.vue
@@ -3,19 +3,19 @@
     <nav><a href="#Toc">Top</a></nav>
     <h2>Labels</h2>
     <Example>
-      <p><span class="label">Label</span></p>
-      <p><span class="label label-success">Success</span></p>
-      <p><span class="label label-info">Info</span></p>
-      <p><span class="label label-warning">Warning</span></p>
-      <p><span class="label label-danger">Danger</span></p>
+      <p><span class="label">Label</span> .label</p>
+      <p><span class="label label-success">Success</span> .label-success</p>
+      <p><span class="label label-info">Info</span> .label-info</p>
+      <p><span class="label label-warning">Warning</span> .label-warning</p>
+      <p><span class="label label-danger">Danger</span> .label-danger</p>
       <p><span class="label">Consecutive</span><span class="label">labels</span></p>
     </Example>
 
     <h3>Sizes</h3>
     <Example>
-      <p>.label <span class="label">Label</span></p>
-      <p>.label-sm <span class="label label-sm">Small</span></p>
-      <p>.label-xs <span class="label label-xs">Extra small</span></p>
+      <p><span class="label">Label</span> .label</p>
+      <p><span class="label label-sm">Small</span> .label-sm</p>
+      <p><span class="label label-xs">Extra small</span> .label-xs</p>
     </Example>
 
   </section>
