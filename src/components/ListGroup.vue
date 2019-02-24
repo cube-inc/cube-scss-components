ade521c867ca1e0470101743fbaaa72c6f55f13b
Fred Gomez <fred.titao@gmail.com>
2019-02-24 10:47:25 +0100

    Improves ListGroup

--- src/components/ListGroup.vue
+++ src/components/ListGroup.vue
@@ -5,16 +5,16 @@
     <Example>
       <ul class="list-group">
         <li class="list-group-item">
-          <div class="list-group-item-body">Default</div>
+          <div class="list-group-item-body">Item 1</div>
         </li>
         <li class="list-group-item">
-          <a class="list-group-item-header">
-            <div class="list-group-item-body">Clickable</div>
+          <a>
+            <div class="list-group-item-body">Item 2 – Clickable</div>
           </a>
         </li>
         <li class="list-group-item">
           <div class="list-group-item-header">
-            <div>With extra actions to the right</div>
+            <div>Item 3 – With actions to the right</div>
             <div class="actions">
               <a>Action 1</a>
               <a>Action 2</a>
@@ -24,21 +24,36 @@
         <li class="list-group-item">
           <div class="list-group-item-header">
             <div class="actions">
-              <a>Action 1</a>
-              <a>Action 2</a>
+              <a>Action</a>
             </div>
-            <div>With extra actions to the left</div>
+            <div>Item 4 – With action to the left</div>
           </div>
         </li>
         <li class="list-group-item">
           <div class="list-group-item-header">
-            <h4>Rich content</h4>
+            <h4>Item 5 – Rich content</h4>
             <div class="actions">
               <a>click me</a>
             </div>
           </div>
-          <div class="list-group-item-body">This is a body</div>
+          <div class="list-group-item-body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio perferendis nesciunt voluptatum, sequi molestias aut tempore temporibus quod non delectus dicta odit, cum eaque quos neque sapiente, dolores ut id.</div>
           <div class="list-group-item-footer">This is a footer</div>
+        </li>
+        <li class="list-group-item">
+          <a>
+            <div class="list-group-item-header">
+              <h4>Item 6 – Clickable</h4>
+            </div>
+            <div class="list-group-item-body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio perferendis nesciunt voluptatum, sequi molestias aut tempore temporibus quod non delectus dicta odit, cum eaque quos neque sapiente, dolores ut id.</div>
+          </a>
+        </li>
+        <li class="list-group-item">
+          <a>
+            <div class="list-group-item-header">
+              <h4>Item 7 – Clickable header</h4>
+            </div>
+          </a>
+          <div class="list-group-item-body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio perferendis nesciunt voluptatum, sequi molestias aut tempore temporibus quod non delectus dicta odit, cum eaque quos neque sapiente, dolores ut id.</div>
         </li>
       </ul>
     </Example>
