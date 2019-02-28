1b6e4b2355725d54a8029e306cd9b45c52156def
Fred Gomez <fred.titao@gmail.com>
2019-02-28 18:20:37 +0100

    Adds Style component

--- src/components.js
+++ src/components.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 
 import Example from './components/Example.vue'
+import Style from './components/Style.vue'
 
 Vue.component(Example.name, Example)
+Vue.component(Style.name, Style)
---
+++ src/components/Style.vue
@@ -0,0 +1,58 @@
+<template>
+  <code class="style code-block" ref="code" v-html="codeHtml"></code>
+</template>
+
+<script>
+import scssVariablesToHtml from '@/services/ScssVariablesService'
+export default {
+  name: 'Style',
+  props: {
+    code: String
+  },
+  computed: {
+    codeHtml () {
+      return scssVariablesToHtml(this.$slots.default)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$style-padding: 16px !default;
+$style-border-color: $component-border-color !default;
+$style-border-radius: $component-border-radius !default;
+$style-code-bg-color: $gray-070 !default;
+
+$code-scss-var-color: $blue;
+$code-scss-val-color: $red;
+
+.style {
+  display: block;
+  padding: $style-padding;
+  border: 1px solid $style-border-color;
+  border-radius: $style-border-radius;
+  background-color: $style-code-bg-color;
+  white-space: pre-wrap;
+}
+/deep/
+.code {
+  &-scss-var {
+    color: $code-scss-var-color;
+  }
+  &-scss-val {
+    color: $gray-900;
+  }
+  &-scss-color {
+    color: #005cc5;
+  }
+  &-scss-num {
+    color: #22863a;
+  }
+  &-scss-string {
+    color: $teal;
+  }
+  &-scss-flag {
+    color: $red;
+  }
+}
+</style>
