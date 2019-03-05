cf182fecd64e65eb0b5721ee763506c6b951f37f
Fred Gomez <fred.titao@gmail.com>
2019-03-05 12:05:34 +0100

    Improves Forms

--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -61,11 +61,78 @@
       </fieldset>
     </Example>
 
+    <h3>Readonly</h3>
     <Example>
       <fieldset class="form-group">
         <label class="form-label">
           Input readonly
           <input type="text" class="form-control" value="This input is readonly" readonly>
+        </label>
+      </fieldset>
+      <fieldset class="form-group">
+        <label class="form-label">
+          Textarea
+          <textarea class="form-control" readonly>This textarea is readonly</textarea>
+        </label>
+      </fieldset>
+    </Example>
+
+    <h3>Disabled</h3>
+    <Example>
+      <fieldset class="form-group">
+        <label class="form-label">
+          Input text
+          <input type="text" class="form-control" placeholder="Type something" disabled>
+        </label>
+      </fieldset>
+      <fieldset class="form-group">
+        <label class="form-label">
+          Input email
+          <input type="email" class="form-control" placeholder="Type an email address" disabled>
+        </label>
+      </fieldset>
+      <fieldset class="form-group">
+        <label class="form-label">
+          Input number
+          <input type="number" class="form-control" placeholder="Type a number" disabled>
+        </label>
+      </fieldset>
+      <fieldset class="form-group">
+        <label class="form-label">
+          Select
+          <select class="form-control" required disabled>
+            <option value hidden>Choose an option</option>
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+          </select>
+        </label>
+      </fieldset>
+      <fieldset class="form-group">
+        <label class="form-label">
+          Textarea
+          <textarea class="form-control" placeholder="Type something…" disabled></textarea>
+        </label>
+      </fieldset>
+      <fieldset class="form-group">
+        <label class="form-label">Input checkbox</label>
+        <label class="form-label">
+          <input type="checkbox" class="form-control" disabled>
+          <span class="form-text">This is a checkbox</span>
+        </label>
+        <label class="form-label">
+          <input type="checkbox" class="form-control" disabled>
+          <span class="form-text">This is another checkbox</span>
+        </label>
+      </fieldset>
+      <fieldset class="form-group">
+        <label class="form-label">Input radio</label>
+        <label class="form-label">
+          <input type="radio" class="form-control" name="input-radio" disabled>
+          <span class="form-text">This is a radio</span>
+        </label>
+        <label class="form-label">
+          <input type="radio" class="form-control" name="input-radio" disabled>
+          <span class="form-text">This is another radio</span>
         </label>
       </fieldset>
     </Example>
